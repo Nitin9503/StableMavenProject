d5bab3d7632244cdfc9f9e57ea4882cd2802d8b3
Arjun Thawkar <arjun@torenzo.com>
2019-01-22 18:34:08 +0530

    appium

--- src/test/java/com/torenzo/qa/testcases/TableStructureOrderTest.java
+++ src/test/java/com/torenzo/qa/testcases/TableStructureOrderTest.java
@@ -1,6 +1,6 @@
 
-
 package com.torenzo.qa.testcases;
+
 import io.appium.java_client.AppiumDriver;
 
 import io.appium.java_client.MobileElement;
@@ -38,110 +38,101 @@ import com.torenzo.qa.util.Reusemethod;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidKeyCode;
 import static com.torenzo.qa.util.StaticVariable.addedGuestToOrder;
-import static com.torenzo.qa.util.StaticVariable.homePageTitle ;
+import static com.torenzo.qa.util.StaticVariable.homePageTitle;
 
 @Listeners(com.torenzo.qa.listener.Listener.class)
 
-public class  TableStructureOrderTest extends Loginapp{
+public class TableStructureOrderTest extends Loginapp {
 
+	Reusemethod call = new Reusemethod();
 
-	 Reusemethod call = new Reusemethod();
-	
-	 	 
 	public TableStructureOrderTest() throws IOException {
 		super();
-	
+
 	}
 
-	@Test(priority=0)
-		public void orderWithTable() throws IOException, InterruptedException
-		{
+	@Test(priority = 0)
+	public void orderWithTable() throws IOException, InterruptedException {
 
-
-		
-         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
-    	try{
-	    	 if(driver.findElement(By.id(obj.getProperty("TableStructureIcon"))).isDisplayed())
-			 {
-			  System.out.println("Reloading table structure");
-		
-			  call.SwapAdmin();
-			/* boolean adminPanelTitile = adminSettingPage.verifyAdminSettingPanel();
-			 Assert.assertTrue(adminPanelTitile, "Admin Panel not found");*/
-			 driver.findElement(By.id(obj.getProperty("AdminSettings"))).click();
-			 /*boolean tableViewPageTitle = adminSettingPage.verifyTableViewDisplay();
-             Assert.assertTrue(tableViewPageTitle, "Table view not found inside Admin Settings after swapping");*/
-		     driver.findElement(By.id(obj.getProperty("ReloadTables"))).click();
-	         }
-				 
-	      }catch (Exception e)
-		  {
-			    System.out.println("verifying table strucuture");
-			    call.SwapAdmin();
-				/*boolean adminPanelTitile = adminSettingPage.verifyAdminSettingPanel();
-				Assert.assertTrue(adminPanelTitile, "Admin Panel not found");*/
-			    driver.findElement(By.id(obj.getProperty("AdminSettings"))).click();
-			    /*boolean tableViewPageTitle = adminSettingPage.verifyTableViewDisplay();
-                Assert.assertTrue(tableViewPageTitle, "Table view not found inside Admin Settings after swapping ");*/
-		        driver.findElement(By.id(obj.getProperty("TableviewDisplay"))).click();
-		      driver.findElement(By.id(obj.getProperty("ReloadTables"))).click();
-	      }
-		}
-	
-	@Test(priority=1)
-	public void tableStrucuterVerify(){
-	
-		/*  boolean titleofTablestructure = tableStructurePage.verifyTableStructurePage();
-		  Assert.assertTrue(titleofTablestructure, "Table structure page is not found after relaoding table from Admin setting");*/
-		 System.out.println("PASS");
-          driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); 
-	    call.checkForEmptyTable();
-	          
-		       }
-
-		
-	@Test(priority=2)
-	 public void addguestandItem() throws IOException, InterruptedException
-	 {
-         
-		System.out.println("selecting guest and adding item to them");
-		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		List<WebElement> guestCountFromOrder = driver.findElements(By.id("guest_name"));		
-		System.out.println("guestCountFromOrder = " +guestCountFromOrder.size());
-		System.out.println("addedGuestToOrder = " +addedGuestToOrder);
+
+		try {
+			if (driver.findElement(By.id(obj.getProperty("TableStructureIcon"))).isDisplayed()) {
+				System.out.println("Reloading table structure");
+
+				call.SwapAdmin();
+				/*
+				 * boolean adminPanelTitile =
+				 * adminSettingPage.verifyAdminSettingPanel();
+				 * Assert.assertTrue(adminPanelTitile, "Admin Panel not found");
+				 */
+				driver.findElement(By.id(obj.getProperty("AdminSettings"))).click();
+				/*
+				 * boolean tableViewPageTitle =
+				 * adminSettingPage.verifyTableViewDisplay();
+				 * Assert.assertTrue(tableViewPageTitle,
+				 * "Table view not found inside Admin Settings after swapping");
+				 */
+				driver.findElement(By.id(obj.getProperty("ReloadTables"))).click();
+			}
+
+		} catch (Exception e) {
+			System.out.println("verifying table strucuture");
+			call.SwapAdmin();
+			/*
+			 * boolean adminPanelTitile =
+			 * adminSettingPage.verifyAdminSettingPanel();
+			 * Assert.assertTrue(adminPanelTitile, "Admin Panel not found");
+			 */
+			driver.findElement(By.id(obj.getProperty("AdminSettings"))).click();
+			/*
+			 * boolean tableViewPageTitle =
+			 * adminSettingPage.verifyTableViewDisplay();
+			 * Assert.assertTrue(tableViewPageTitle,
+			 * "Table view not found inside Admin Settings after swapping ");
+			 */
+			driver.findElement(By.id(obj.getProperty("TableviewDisplay"))).click();
+			driver.findElement(By.id(obj.getProperty("ReloadTables"))).click();
+		}
+	}
+
+	@Test(priority = 1)
+	public void tableStrucuterVerify() {
+
+		/*
+		 * boolean titleofTablestructure =
+		 * tableStructurePage.verifyTableStructurePage();
+		 * Assert.assertTrue(titleofTablestructure,
+		 * "Table structure page is not found after relaoding table from Admin setting"
+		 * );
+		 */
+		System.out.println("PASS");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		call.checkForEmptyTable();
+
+	}
+
+	@Test(priority = 2)
+	public void addguestandItem() throws IOException, InterruptedException {
+
+		System.out.println("selecting guest and adding item to them");
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		List<WebElement> guestCountFromOrder = driver.findElements(By.id("guest_name"));
+		System.out.println("guestCountFromOrder = " + guestCountFromOrder.size());
+		System.out.println("addedGuestToOrder = " + addedGuestToOrder);
 		String change = Integer.toString(guestCountFromOrder.size());
-		System.out.println("change = " +change);
-		Assert.assertEquals(addedGuestToOrder, change, "Guest are not matched to each other after adding from party size to order page");
+		System.out.println("change = " + change);
+		Assert.assertEquals(addedGuestToOrder, change,
+				"Guest are not matched to each other after adding from party size to order page");
 		call.selectGuestandAddItem();
-	
-	
-	 }
-      	
-			@Test(priority=3)
-			 public void paymentorder() throws IOException, InterruptedException
-			 {
-				call.Payment();
-		        System.out.println("Table structure class end here");		
-				}
-			
+
+	}
+
+	@Test(priority = 3)
+	public void paymentorder() throws IOException, InterruptedException {
+		call.Payment();
+		System.out.println("Table structure class end here");
+	}
+
 }
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	
